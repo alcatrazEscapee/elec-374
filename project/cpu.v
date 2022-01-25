c9f7585f0da2aae2df7a108bf8dc938eeb7799bc
AlcatrazEscapee <alex@molleroneill.com>
2022-01-25 07:59:07 -0500

    Move testbench to the same module it's target is declared in, and add dummy logic in cpu.v to create the hiearchy correctly. Testbench works?

--- project/cpu.v
+++ project/cpu.v
@@ -1,3 +1,4 @@
-module cpu;
-
+module cpu( input [37:0] dummy_in, output [31:0] dummy_out );
+	// Top level module needs to contain some logic, and instantiate the design?
+	alu_shift_left shl (.in (dummy_in[31:0]), .shift(dummy_in[37:32]), .out(dummy_out) );
 endmodule
