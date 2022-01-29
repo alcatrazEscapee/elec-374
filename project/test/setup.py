8b703ade4f06bd25bddde72af5235a17858478e7
AlcatrazEscapee <alex@molleroneill.com>
2022-01-28 23:03:12 -0500

    Include module name in test failure messages

--- project/test/setup.py
+++ project/test/setup.py
@@ -13,13 +13,17 @@ def mock(expected, actual, message):
 def main():
     print('Analyzing Test Results')
     log = sys.argv[1]
+    target = '???'
     with open(log, 'r', encoding='utf-8') as f:
         i = 0
         for output in f.read().split('\n'):
+            if output.startswith('vsim -voptargs=+acc work.'):
+                *_, target = output.split('.')
+                i = 0
             if output.startswith('# Test'):
                 _, name, expected, actual = map(lambda x: ' '.join(x.split()), output.split('|'))
                 test = mock(expected, actual, '%s : Expected %s : Actual %s' % (name, expected, actual))
-                test.__name__ = 'test %d: %s' %(i, name)
+                test.__name__ = 'test %d : %s : %s' %(i, target, name)
                 setattr (ModelSim, test.__name__, test)
                 i += 1
     sys.argv = sys.argv[:1]
