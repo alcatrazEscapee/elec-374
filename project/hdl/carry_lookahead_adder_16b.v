75d46ab4f95ed1df484bddfc6b6ff07e71d19232
AlcatrazEscapee <alex@molleroneill.com>
2022-01-30 08:36:39 -0500

    Add a 16-bit hierarchical CLA, and a 16N-bit RCA using underlying CLAs.

---
+++ project/hdl/carry_lookahead_adder_16b.v
@@ -0,0 +1,44 @@
+module carry_lookahead_adder_16b(
+	input [15:0] a,
+	input [15:0] b,
+	output [15:0] sum,
+	input c_in,
+	output c_out
+);
+	wire g0, g1, g2, g3, p0, p1, p2, p3, c0, c1, c2, c3, c01, c02, c03;
+		
+	// Carry Lookahead
+	assign c3 = g3 | (p3 & g2) | (p3 & p2 & g1) | (p3 & p2 & p1 & g0) | (p3 & p2 & p1 & p0 & c_in);
+	assign c2 = g2 | (p2 & g1) | (p2 & p1 & g0) | (p2 & p1 & p0 & c_in);
+	assign c1 = g1 | (p1 & g0) | (p1 & p0 & c_in);
+	assign c0 = g0 | (p0 & c_in);
+	
+	// Sum + Carry
+	carry_lookahead_adder_4b _cla0 ( .a(a[3:0]), .b(b[3:0]), .sum(sum[3:0]), .c_in(c_in), .c_out(c01), .p_out(p0), .g_out(g0) );
+	carry_lookahead_adder_4b _cla1 ( .a(a[7:4]), .b(b[7:4]), .sum(sum[7:4]), .c_in(c0), .c_out(c02), .p_out(p1), .g_out(g1) );
+	carry_lookahead_adder_4b _cla2 ( .a(a[11:8]), .b(b[11:8]), .sum(sum[11:8]), .c_in(c1), .c_out(c03), .p_out(p2), .g_out(g2) );
+	carry_lookahead_adder_4b _cla3 ( .a(a[15:12]), .b(b[15:12]), .sum(sum[15:12]), .c_in(c2), .c_out(c_out), .p_out(p3), .g_out(g3) );
+
+endmodule
+
+
+`timescale 1ns/100ps
+module carry_lookahead_adder_16b_test;
+	
+	reg [16:0] a, b;
+	reg c_in;
+	wire [16:0] sum;
+	
+	integer i;
+	
+	carry_lookahead_adder_16b cla ( .a(a[15:0]), .b(b[15:0]), .sum(sum[15:0]), .c_in(c_in), .c_out(sum[16]) );
+	
+	initial begin
+		for (i = 0; i < 1000; i = i + 1) begin
+			a <= $urandom % 65536;
+			b <= $urandom % 65536;
+			c_in <= $urandom;
+			#1 $display("Test | add %0d + %0d + %0d | %0d | %0d", a, b, c_in, a + b + c_in, sum);
+		end
+	end
+endmodule
--- project/hdl/carry_lookahead_adder_4b.v
+++ project/hdl/carry_lookahead_adder_4b.v
@@ -3,23 +3,28 @@ module carry_lookahead_adder_4b(
 	input [3:0] b,
 	output [3:0] sum,
 	input c_in,
-	output c_out
+	output c_out,
+	output g_out,
+	output p_out
 );
-	wire g0, g1, g2, g3, p0, p1, p2, p3, c0, c1, c2, c3;
+	wire g0, g1, g2, g3, p0, p1, p2, p3, c0, c1, c2;
 	
 	assign {g3, g2, g1, g0} = a & b; // gi = ai & bi
 	assign {p3, p2, p1, p0} = a | b; // pi = ai | bi
 	
 	// Carry Lookahead
-	assign c3 = g3 | (p3 & g2) | (p3 & p2 & g1) | (p3 & p2 & p1 & g0) | (p3 & p2 & p1 & p0 & c_in);
 	assign c2 = g2 | (p2 & g1) | (p2 & p1 & g0) | (p2 & p1 & p0 & c_in);
-	assign c1 = g1 | (p1 & g0) | (p1 & p2 & c_in);
+	assign c1 = g1 | (p1 & g0) | (p1 & p0 & c_in);
 	assign c0 = g0 | (p0 & c_in);
 	
+	// Generate + Propogate out
+	assign p_out = p3 & p2 & p1 & p0;
+	assign g_out = g3 | (p3 & g2) | (p3 & p2 & g1) | (p3 & p2 & p1 & g0);
+
 	// Sum + Carry
 	assign sum = a ^ b ^ {c2, c1, c0, c_in};
-	assign c_out = c3;
-
+	assign c_out = g_out | (p_out & c_in);
+	
 endmodule
 
 
@@ -29,18 +34,22 @@ module carry_lookahead_adder_4b_test;
 	reg [3:0] a, b;
 	reg c_in;
 	wire [4:0] sum;
+	wire p_out, g_out;
 	
-	integer i, j;
+	integer i, j, k;
 	
-	carry_lookahead_adder_4b cla ( .a(a), .b(b), .sum(sum[3:0]), .c_in(c_in), .c_out(sum[4]) );
+	carry_lookahead_adder_4b cla ( .a(a), .b(b), .sum(sum[3:0]), .c_in(c_in), .c_out(sum[4]), .p_out(p_out), .g_out(g_out) );
 	
 	initial begin
 		c_in <= 0;
 		for (i = 0; i < 16; i = i + 1) begin
 			for (j = 0; j < 16; j = j + 1) begin
-				a <= i;
-				b <= j;
-				#1 $display("Test | add %0d + %0d | %0d | %0d", i, j, i + j, sum);
+				for (k = 0; k <= 1; k = k + 1) begin
+					a <= i;
+					b <= j;
+					c_in <= k;
+					#1 $display("Test | add %0d + %0d + %0d | %0d | %0d", i, j, k, i + j + k, sum);
+				end
 			end
 		end
 	end
