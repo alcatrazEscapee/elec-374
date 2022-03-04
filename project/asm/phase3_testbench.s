ed74939134b1bef483c4a3183f89fa19e883fdf8
AlcatrazEscapee <alex@molleroneill.com>
2022-03-04 13:17:11 -0500

    Add Phase 3 specification and assembly testbench (translated).

---
+++ project/asm/phase3_testbench.s
@@ -0,0 +1,54 @@
+// Phase 3 Example Program
+// All '$X' constants are interpreted as hex
+// The return address register was changed from r14 to r15, in compliance with the spec.
+
+.org 0
+    ldi r3, 0x87        // r3 = 0x87
+    ldi r3, 1(r3)       // r3 = 0x88
+    ld r2, 0x75         // r2 = (0x75) = 0x56
+    ldi r2, -2(r2)      // r2 = 0x54
+    ld r1, 4(r2)        // r1 = (0x58) = 0x34
+    ldi r0, 1           // r0 = 1
+    ldi r3, 0x73        // r3 = 0x73
+    brmi r3, 3          // branch not taken
+    ldi r3, 5(r3)       // r3 = 0x78
+    ld r7, -3(r3)       // r7 = (0x78 - 3) = 0x56
+    nop
+    brpl r7, 2          // branch to target
+    ldi r4, 6(r1)       // skipped
+    ldi r3, 2(r4)       // skipped
+target:
+    add r3, r2, r3      // r3 = 0xCC
+    addi r7, r7, 3      // r7 = 0x59
+    neg r7, r7          // r7 = 0xFFFFFFA7
+    not r7, r7          // r7 = 0x58
+    andi r7, r7, 0x0F   // r7 = 8
+    ori r7, r1, 3       // r7 = 0x37
+    shr r2, r3, r0      // r2 = 0x66
+    st 0x58, r2         // Memory[0x58] = 0x66
+    ror r1, r1, r0      // r1 = 0x1A
+    rol r2, r2, r0      // r2 = 0xCC
+    or r2, r3, r0       // r2 = 0xCD
+    and r1, r2, r1      // r1 = 0x8
+    st 0x67(r1), r2     // Memory[0x75] = 0xCD
+    sub r3, r2, r3      // r3 = 1
+    shl r1, r2, r0      // r1 = 0x19A
+    ldi r4, 5           // r4 = 5
+    ldi r5, 0x1D        // r5 = 0x1D
+    mul r5, r4          // {HI, LO} = 0, 0x91
+    mfhi r7             // r7 = 0
+    mflo r6             // r6 = 0x91
+    div r5, r4          // {HI, LO} = 4, 5
+    ldi r10, 0(r4)      // r10 = 5 setting up argument registers
+    ldi r11, 2(r5)      // r11 = 0x1F r8, r9, r10, and r11
+    ldi r12, 0(r6)      // r12 = 0x91
+    ldi r13, 0(r7)      // r13 = 0
+    jal r12             // address of subroutine subA in r12 - return address in r15
+    halt                // upon return, the program halts
+
+.org 0x91               // procedure subA
+subA:
+    add r9, r10, r12    // r8 and r9 are return value registers
+    sub r8, r11, r13    // r9 = 0x96, r8 = 0x1F
+    sub r9, r9, r8      // r13 = 0x77
+    jr r15              // return
